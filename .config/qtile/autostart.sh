f479b3fd5aa46eee358ba460a4dd3090f0554323
Marty1820 <nickm1820@gmail.com>
2021-12-06 20:44:50 -0600

    qtile configs/I'm better at python than lua

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash 
+
+conky &
+feh --bg-fill --randomize ~/Pictures/wallpapers/* &
+picom &
---
+++ .config/qtile/config.py
@@ -0,0 +1,401 @@
+# ███╗░░░███╗░█████╗░██████╗░████████╗██╗░░░██╗
+# ████╗░████║██╔══██╗██╔══██╗╚══██╔══╝╚██╗░██╔╝
+# ██╔████╔██║███████║██████╔╝░░░██║░░░░╚████╔╝░
+# ██║╚██╔╝██║██╔══██║██╔══██╗░░░██║░░░░░╚██╔╝░░
+# ██║░╚═╝░██║██║░░██║██║░░██║░░░██║░░░░░░██║░░░
+# ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░░░╚═╝░░░
+#
+# qtile config
+# -*- coding: utf-8 -*-
+
+# IMPORTS
+import os
+from typing import List  # noqa: F401
+
+from libqtile import qtile, bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, KeyChord, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+mod = "mod4"				# Sets mod key to SUPER
+myTerm = "alacritty"
+myBrowser = "vivaldi-stable"
+myFilemgr = "pcmanfm"
+myEditor = "gedit"
+#screenshot = "scrot -e 'mv $f ~/Pictures/Screenshot 2>/dev/null'"
+lock = "dm-tool lock"
+
+keys = [
+	### Essentials
+    Key([mod], "Return",
+        lazy.spawn(myTerm),
+        desc="Launches My Terminal"
+        ),
+    Key([mod], "b",
+        lazy.spawn(myBrowser),
+        desc="Launches My Web Browser"
+        ),
+    Key([mod], "e",
+        lazy.spawn(myFilemgr),
+        desc="Launches My File Manager"
+        ),
+    Key([mod], "a",
+        lazy.spawn(myEditor),
+        desc="Launches Text Editor"
+        ),
+    Key([mod, "shift"], "q",
+        lazy.shutdown(),
+        desc="Shutdown Qtile"
+        ),
+    Key([mod, "shift"], "r",
+        lazy.restart(),
+        desc="Restart Qtile"
+        ),
+    Key([mod, "shift"], "x",
+    	lazy.spawn(lock),
+    	desc="Locks computer"
+    	),
+    Key([mod, "shift"], "c",
+        lazy.window.kill(),
+        desc="Kill focused window"
+        ),
+    Key([mod], "space",
+        lazy.next_layout(),
+        desc="Toggle between layouts"
+        ),
+    # Switch between windows
+    Key([mod], "h",
+        lazy.layout.left(),
+        desc="Move focus to left"
+        ),
+    Key([mod], "l",
+        lazy.layout.right(),
+        desc="Move focus to right"
+        ),
+    Key([mod], "j",
+        lazy.layout.up(),
+        desc="Move focus up"
+        ),
+    Key([mod], "k",
+        lazy.layout.down(),
+        desc="Move focus down"
+        ),
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h",
+        lazy.layout.shuffle_left(),
+        desc="Move window to the left"
+        ),
+    Key([mod, "shift"], "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right"
+        ),
+    Key([mod, "shift"], "j",
+        lazy.layout.shuffle_up(),
+        desc="Move window up"
+        ),
+    Key([mod, "shift"], "k",
+    	lazy.layout.shuffle_down(),
+    	desc="Move window down"
+    	),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h",
+    	lazy.layout.grow_left(),
+        desc="Grow window to the left"
+        ),
+    Key([mod, "control"], "l",
+    	lazy.layout.grow_right(),
+        desc="Grow window to the right"
+        ),
+    Key([mod, "control"], "j",
+    	lazy.layout.grow_up(),
+        desc="Grow window up"
+        ),
+    Key([mod, "control"], "k",
+    	lazy.layout.grow_down(),
+    	desc="Grow window down"
+    	),
+    Key([mod], "n",
+    	lazy.layout.normalize(),
+    	desc="Reset all window sizes"
+    	),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key([mod, "shift"], "Return",
+    	lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"
+        ),
+    
+    
+    Key([mod], "r",
+    	lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"
+        ),
+]
+
+
+groups = [Group("DEV"),
+          Group("WWW"),
+          Group("SYS"),
+          Group("DOC"),
+          Group("VBOX"),
+          Group("CHAT"),
+          Group("MUS"),
+          Group("VID"),
+          Group("GFX")]
+
+# Allow MODKEY+[0 through 9] to bind to groups, see https://docs.qtile.org/en/stable/manual/config/groups.html
+# MOD4 + index Number : Switch to Group[index]
+# MOD4 + shift + index Number : Send active window to another Group
+from libqtile.dgroups import simple_key_binder
+dgroups_key_binder = simple_key_binder("mod4")
+
+layout_theme = {"border_width": 2,
+                "margin": 8,
+                "border_focus": "bd93f9",
+                "border_normal": "282a36"
+                }
+
+
+layouts = [
+    layout.Columns(border_focus_stack=['#bd93f9', '#ff5555'], border_width=2),
+    layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+    layout.Floating(),
+]
+
+colors = [["#282a36"], # Background Dark Grey
+          ["#44475a"], # Current Line/Selection Grey
+          ["#f8f8f2"], # Foreground Whitish
+          ["#6272a4"], # Comment Blue/Grey
+          ["#8be9fd"], # Cyan
+          ["#50fa7b"], # Green
+          ["#ffb86c"], # Orange
+          ["#ff79c6"], # Pink
+          ["#bd93f9"], # Purple
+          ["#ff5555"], # Red
+          ["#f1fa8c"]] # Yellow
+
+widget_defaults = dict(
+    font='mononoki Nerd Font Mono',
+    fontsize=22,
+    padding=2,
+    background = colors[0]
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+				widget.GroupBox(
+                    active = colors[2],
+                    block_highlight_text_color = colors[8],
+                    highlight_method = "line",
+                    ),
+                widget.Prompt(),
+                widget.Sep(
+                	linewidth = 0,
+                    padding = 40,
+                    foreground = colors[2],
+                    background = colors[0],
+                    ),
+                widget.WindowName(
+                	format = '{name}',
+                	max_chars = 20,
+                	),
+                widget.TextBox(
+                	text = '',
+                	foreground = colors[3],
+                	background = colors[0],
+                	padding = 0,
+                	fontsize = 28,
+                	),
+                widget.Systray(
+                	icon_size = 26,
+                	padding = 10,
+                	foreground = colors[0],
+                	background = colors[3],
+                	),
+                widget.TextBox(
+                	text = '',
+                	foreground = colors[9],
+                	background = colors[3],
+                	padding = 0,
+                	fontsize = 28,
+                	),
+              widget.TextBox(
+                    text = "⟳ ",
+                    padding = 2,
+                    foreground = colors[0],
+                    background = colors[9],
+                    ),
+              widget.CheckUpdates(
+                    update_interval = 1800,
+                    distro = "Arch_checkupdates",
+                    display_format = "{updates} Updates",
+                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e sudo pacman -Syu')},
+                    foreground = colors[0],
+                    background = colors[9],
+                    ),
+                widget.TextBox(
+                	text = '',
+                	foreground = colors[5],
+                	background = colors[9],
+                	padding = 0,
+                	fontsize = 28,
+                	),
+                widget.Net(
+                	interface = "wlp170s0",
+                	format = '{down}↓↑{up}',
+                	padding = 5,
+                	foreground = colors[0],
+                	background = colors[5],
+                	),
+                widget.TextBox(
+                	text = '',
+                	foreground = colors[6],
+                	background = colors[5],
+                	padding = 0,
+                	fontsize = 28,
+                	),
+              	widget.ThermalSensor(
+                    threshold = 90,
+                    padding = 5,
+                    foreground = colors[0],
+                    background = colors[6]
+                    ),
+                widget.TextBox(
+                	text = '',
+                	foreground = colors[7],
+                	background = colors[6],
+                	padding = 0,
+                	fontsize = 28,
+                	),
+              	widget.Memory(
+                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e btop')},
+                    padding = 5,
+                    foreground = colors[0],
+                    background = colors[7],
+                    ),
+                widget.TextBox(
+                	text = '',
+                	foreground = colors[8],
+                	background = colors[7],
+                	padding = 0,
+                	fontsize = 28,
+                	),
+                widget.TextBox(
+                	text = 'Vol:',
+                	foreground = colors[0],
+                	background = colors[8],
+                	padding = 0,
+                	),
+              	widget.Volume(
+                    padding = 5,
+                    foreground = colors[0],
+                    background = colors[8],
+                    ),
+				widget.TextBox(
+                	text = '',
+                	foreground = colors[2],
+                	background = colors[8],
+                	padding = 0,
+                	fontsize = 28,
+                	),
+                widget.Clock(
+                	format='%a, %b %d %I:%M %p',
+                	foreground = colors[0],
+                	background = colors[2],
+                	),
+               	widget.TextBox(
+                	text = '',
+                	foreground = colors[10],
+                	background = colors[2],
+                	padding = 0,
+                	fontsize = 28,
+                	),
+                widget.Battery(
+                	charge_char = '',
+                	discharge_char = '',
+                	empty_char = '',
+                	full_char = '',
+                	format = '{char}{percent:2.0%}',
+                	foreground = colors[0],
+                	background = colors[10],
+                	),
+				widget.CurrentLayout(),
+            ],
+            24,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = True
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+    Match(title='conky'),
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+def window_to_prev_group(qtile):
+	if qtile.currentWindow is not None:
+		i = qtile.groups.index(qtile.currentGroup)
+
+def window_to_next_group(qtile):
+    if qtile.currentWindow is not None:
+        i = qtile.groups.index(qtile.currentGroup)
+        qtile.currentWindow.togroup(qtile.groups[i + 1].name)
+
+
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser('~')
+    subprocess.call([home + '/.config/qtile/autostart.sh'])
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
