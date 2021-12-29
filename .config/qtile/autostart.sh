17340b52239b42c1d02c83f9b7fa70e9098dba4b
Marty1820 <nickm1820@gmail.com>
2021-12-28 18:53:51 -0600

    added services/reordered

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash 
 
 picom &
-feh --bg-fill --randomize ~/Pictures/wallpapers/FrameWork/* &
+gnome-keyring-daemon &
+blueman-applet &
 conky &
 redshift &
 xfce4-power-manager &
 light-locker &
 /usr/lib/kdeconnectd &
+feh --bg-fill --randomize ~/Pictures/wallpapers/FrameWork/* &
