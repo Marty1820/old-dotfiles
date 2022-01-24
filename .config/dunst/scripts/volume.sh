e9da740def35f3eb8670a5d8f35197065079da54
Marty1820 <nickm1820@gmail.com>
2022-01-23 20:58:52 -0600

    volume notification with dunst

---
+++ .config/dunst/scripts/volume.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# You can call this script like this:
+# $./volume.sh up
+# $./volume.sh down
+# $./volume.sh mute
+
+function get_volume {
+    amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
+}
+
+function is_mute {
+    amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
+}
+
+function send_notification {
+    volume=`get_volume`
+    # Make the bar with the special character ─ (it's not dash -)
+    # https://en.wikipedia.org/wiki/Box-drawing_character
+    bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
+    # Send the notification
+    if [ "$volume" = "0" ]; then
+    dunstify --raw_icon=/usr/share/icons/Adwaita/48x48/status/audio-volume-muted-symbolic.symbolic.png --timeout=4500 --replace=2593 --urgency=normal "$volume    $bar"
+    elif [ "$volume" -lt "30" ]; then
+    dunstify --raw_icon=/usr/share/icons/Adwaita/48x48/status/audio-volume-low-symbolic.symbolic.png --timeout=4500 --replace=2593 --urgency=normal "$volume    $bar"
+    elif [ "$volume" -lt "80" ]; then
+    dunstify --raw_icon=/usr/share/icons/Adwaita/48x48/status/audio-volume-medium-symbolic.symbolic.png --timeout=4500 --replace=2593 --urgency=normal "$volume    $bar"
+    else
+    dunstify --raw_icon=/usr/share/icons/Adwaita/48x48/status/audio-volume-high-symbolic.symbolic.png --timeout=4500 --replace=2593 --urgency=normal "$volume    $bar"
+    fi
+}
+
+case $1 in
+    up)
+	# Up the volume (+ 5%)
+	amixer -q set Master 5%+ unmute > /dev/null
+	send_notification
+	;;
+    down)
+	amixer -q set Master 5%- unmute > /dev/null
+	send_notification
+	;;
+    mute)
+    	# Toggle mute
+	amixer -q set Master 1+ toggle > /dev/null
+	if is_mute ; then
+	    dunstify --raw_icon=/usr/share/icons/Adwaita/48x48/status/audio-volume-muted-symbolic.symbolic.png --timeout=4500 --replace=2593 --urgency=normal "Mute"
+	else
+	    send_notification
+	fi
+	;;
+esac
