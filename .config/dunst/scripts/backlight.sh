e8303959809fb96282d450d4ea5035d2718af57b
Marty1820 <nickm1820@gmail.com>
2022-01-30 13:04:03 -0600

    added brightness curve

--- .config/dunst/scripts/backlight.sh
+++ .config/dunst/scripts/backlight.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
+# Dependancies 'xorg-xbacklight' & 'dunst'
 # You can call this script like this:
 # $./backlight.sh up
 # $./backlight.sh down
 
+# Gets brightness percent from 'xbacklight'
 function get_backlight {
     fullnum=$(xbacklight -get)
     printf %.0f $fullnum
 }
 
+# Changes percent increase/decrease 
+# depending on current brightness & direction
+function set_curve() {
+    backlight=`get_backlight`
+    local direction="$1"
+    if [ $direction = up ]; then
+        if [ $backlight -lt 10 ] ; then
+            percent=1
+        elif [ $backlight -lt 50 ] ; then
+            percent=5
+        else
+            percent=10
+        fi
+    else
+        if [ $backlight -le 10 ] ; then
+            percent=1
+        elif [ $backlight -le 50 ] ; then
+            percent=5
+        else
+            percent=10
+        fi
+    fi
+}
+
+# Sends notification with dunst and set progress bar
 function send_notification {
     backlight=`get_backlight`
     # Make the bar with the special character ─ (it's not dash -)
@@ -20,12 +47,13 @@ function send_notification {
 
 case $1 in
     up)
-	# Up the volume (+ 5%)
-	xbacklight -inc 5 > /dev/null
+    set_curve "up"
+	xbacklight -inc $percent > /dev/null
 	send_notification
 	;;
     down)
-	xbacklight -dec 5 > /dev/null
+    set_curve "down"
+	xbacklight -dec $percent > /dev/null
 	send_notification
 	;;
 esac
