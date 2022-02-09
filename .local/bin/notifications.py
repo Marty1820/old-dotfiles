70043e5dd0cb2cfe6358d255de647c2b72024b85
Marty1820 <nickm1820@gmail.com>
2022-02-08 19:58:29 -0600

    fixed errors & depreciated code

--- .local/bin/notifications.py
+++ .local/bin/notifications.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # pacman -S libappindicator-gtk3
-import os
+import os, gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk, AppIndicator3 as appindicator
 
 def main():
@@ -13,15 +15,15 @@ def main():
 def menu():
     menu = gtk.Menu()
     
-    command_one = gtk.MenuItem("Toggle DUNST")
+    command_one = gtk.MenuItem.new_with_label("Toggle DUNST")
     command_one.connect('activate', toggle)
     menu.append(command_one)
     
-    command_two = gtk.MenuItem('Last Notification')
+    command_two = gtk.MenuItem.new_with_label('Last Notification')
     command_two.connect('activate', notification)
     menu.append(command_two)
     
-    exittray = gtk.MenuItem('Exit Tray')
+    exittray = gtk.MenuItem.new_with_label('Exit Tray')
     exittray.connect('activate', quit)
     menu.append(exittray)
     
