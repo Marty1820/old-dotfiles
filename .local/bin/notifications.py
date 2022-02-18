5a865caa8003800866354835181295b3d9fc6e36
Marty1820 <nickm1820@gmail.com>
2022-02-18 14:06:07 -0600

    added showing full history and closing all notifications

--- .local/bin/notifications.py
+++ .local/bin/notifications.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 # pacman -S libappindicator-gtk3
 import os, gi
 gi.require_version('Gtk', '3.0')
@@ -6,38 +6,53 @@ gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk, AppIndicator3 as appindicator
 
 def main():
-    indicator = appindicator.Indicator.new("customtray", "preferences-system-notifications-symbolic",
-    appindicator.IndicatorCategory.APPLICATION_STATUS)
-    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(menu())
+    ind = appindicator.Indicator.new(
+        "Notifications",
+        "indicator-messages",
+        appindicator.IndicatorCategory.APPLICATION_STATUS
+    )
+    ind.set_status (appindicator.IndicatorStatus.ACTIVE)
+    ind.set_menu(menu())
     gtk.main()
 
 def menu():
     menu = gtk.Menu()
     
-    command_one = gtk.MenuItem.new_with_label("Toggle DUNST")
-    command_one.connect('activate', toggle)
-    menu.append(command_one)
+    cmd_tog = gtk.MenuItem.new_with_label("Pause/Un-pause")
+    cmd_tog.connect('activate', toggle)
+    menu.append(cmd_tog)
     
-    command_two = gtk.MenuItem.new_with_label('Last Notification')
-    command_two.connect('activate', notification)
-    menu.append(command_two)
+    cmd_hist = gtk.MenuItem.new_with_label('Notification History')
+    cmd_hist.connect('activate', notification)
+    menu.append(cmd_hist)
     
-    exittray = gtk.MenuItem.new_with_label('Exit Tray')
-    exittray.connect('activate', quit)
-    menu.append(exittray)
+    cmd_close = gtk.MenuItem.new_with_label('Close All Notifications')
+    cmd_close.connect('activate', close)
+    menu.append(cmd_close)
+    
+    cmd_exit = gtk.MenuItem.new_with_label('Exit Tray')
+    cmd_exit.connect('activate', quit)
+    menu.append(cmd_exit)
     
     menu.show_all()
     return menu
 
 def toggle(_):
-    os.system("dunstctl set-paused toggle")
+    os.system("""dunstctl set-paused toggle
+if [[ $(dunstctl is-paused) = "false" ]]; then
+    dunstify "Notifications" "Notifications are now un-paused"
+fi""")
 
 def notification(_):
-    os.system("dunstctl history-pop")
+    os.system("""while read; do dunstctl history-pop; done <<<$(dunstctl history|grep '"id" :')
+""")
+
+def close(_):
+    os.system("dunstctl close-all")
 
 def quit(_):
     gtk.main_quit()
+    
 
 if __name__ == "__main__":
     main()
