7f124a8b3db1d467b12588ec8894b86309b03e70
Marty1820 <nickm1820@gmail.com>
2022-06-13 17:41:28 -0500

    adding Sway and Waybar configs

---
+++ .config/waybar/modules/pkgs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pac=$(checkupdates 2>/dev/null | wc -l)
+total=$pac
+percentage=$total
+
+if [[ $total -eq 0 ]]; then
+  text="📦"
+  tooltip="📦 Repository Sync\nNo new packages available"
+  class=""
+else
+  text="$total🎁"
+  tooltip="🎁 Repository Sync\n$total Updates available"
+  class="critical"
+fi
+
+echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\", \"class\": \"$class\", \"percentage\": $percentage }"
