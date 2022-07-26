c938c35556ffa072b390ec03a178ab31a43456a6
Marty1820 <nickm1820@gmail.com>
2022-07-26 15:09:46 -0500

    fixed spacing

--- .local/bin/notifications.py
+++ .local/bin/notifications.py
@@ -6,53 +6,52 @@ gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk, AppIndicator3 as appindicator
 
 def main():
-    ind = appindicator.Indicator.new(
-        "Notifications",
-        "indicator-messages",
-        appindicator.IndicatorCategory.APPLICATION_STATUS
-    )
-    ind.set_status (appindicator.IndicatorStatus.ACTIVE)
-    ind.set_menu(menu())
-    gtk.main()
+  ind = appindicator.Indicator.new(
+    "Notifications",
+    "indicator-messages",
+    appindicator.IndicatorCategory.APPLICATION_STATUS
+  )
+  ind.set_status (appindicator.IndicatorStatus.ACTIVE)
+  ind.set_menu(menu())
+  gtk.main()
 
 def menu():
-    menu = gtk.Menu()
-    
-    cmd_tog = gtk.MenuItem.new_with_label("Pause/Un-pause")
-    cmd_tog.connect('activate', toggle)
-    menu.append(cmd_tog)
-    
-    cmd_hist = gtk.MenuItem.new_with_label('Notification History')
-    cmd_hist.connect('activate', notification)
-    menu.append(cmd_hist)
-    
-    cmd_close = gtk.MenuItem.new_with_label('Close All Notifications')
-    cmd_close.connect('activate', close)
-    menu.append(cmd_close)
-    
-    cmd_exit = gtk.MenuItem.new_with_label('Exit Tray')
-    cmd_exit.connect('activate', quit)
-    menu.append(cmd_exit)
-    
-    menu.show_all()
-    return menu
+  menu = gtk.Menu()
+
+  cmd_tog = gtk.MenuItem.new_with_label("Pause/Un-pause")
+  cmd_tog.connect('activate', toggle)
+  menu.append(cmd_tog)
+
+  cmd_hist = gtk.MenuItem.new_with_label('Notification History')
+  cmd_hist.connect('activate', notification)
+  menu.append(cmd_hist)
+
+  cmd_close = gtk.MenuItem.new_with_label('Close All Notifications')
+  cmd_close.connect('activate', close)
+  menu.append(cmd_close)
+
+  cmd_exit = gtk.MenuItem.new_with_label('Exit Tray')
+  cmd_exit.connect('activate', quit)
+  menu.append(cmd_exit)
+
+  menu.show_all()
+  return menu
 
 def toggle(_):
-    os.system("""dunstctl set-paused toggle
+  os.system("""dunstctl set-paused toggle
 if [[ $(dunstctl is-paused) = "false" ]]; then
-    dunstify "Notifications" "Notifications are now un-paused"
+  dunstify "Notifications" "Notifications are now un-paused"
 fi""")
 
 def notification(_):
-    os.system("""while read; do dunstctl history-pop; done <<<$(dunstctl history|grep '"id" :')
+  os.system("""while read; do dunstctl history-pop; done <<<$(dunstctl history|grep '"id" :')
 """)
 
 def close(_):
-    os.system("dunstctl close-all")
+  os.system("dunstctl close-all")
 
 def quit(_):
-    gtk.main_quit()
-    
+  gtk.main_quit()
 
 if __name__ == "__main__":
-    main()
+  main()
--- .local/bin/pokemon-colorscripts.sh
+++ .local/bin/pokemon-colorscripts.sh
@@ -9,82 +9,82 @@ POKEART_DIR="$PROGRAM_DIR/colorscripts"
 # formatting for the help strings
 
 _help(){
-    #Function that prints out the help text
+  #Function that prints out the help text
 
-    echo "Description: CLI utility to print out unicode image of a pokemon in your shell"
-    echo ""
-    echo "Usage: pokemon-colorscripts [OPTION] [POKEMON NAME]"
-    printf "  %-20s\t%-54s\n" \
-        "-h, --help, help" "Print this help." \
-        "-l, --list, list" "Print list of all pokemon"\
-        "-r, --random, random" "Show a random pokemon"\
-        "-n, --name" "Select pokemon by name. 
-        Generally spelled like in the games. 
-        Some exceptions are nidoran-f,nidoran-m,mr-mime,farfetchd,flabebe type-null etc. 
-        grep the output of --list if in doubt"
-    echo "Example: 'pokemon-colorscripts --name pikachu'"
+  echo "Description: CLI utility to print out unicode image of a pokemon in your shell"
+  echo ""
+  echo "Usage: pokemon-colorscripts [OPTION] [POKEMON NAME]"
+  printf "  %-20s\t%-54s\n" \
+    "-h, --help, help" "Print this help." \
+    "-l, --list, list" "Print list of all pokemon"\
+    "-r, --random, random" "Show a random pokemon"\
+    "-n, --name" "Select pokemon by name.
+    Generally spelled like in the games.
+    Some exceptions are nidoran-f,nidoran-m,mr-mime,farfetchd,flabebe type-null etc.
+    grep the output of --list if in doubt"
+  echo "Example: 'pokemon-colorscripts --name pikachu'"
 }
 
 _show_random_pokemon(){
-    #selecting a random art file from the whole set
+  #selecting a random art file from the whole set
 
-    # total number of art files present
-    NUM_ART=$(find "$POKEART_DIR"|wc -l| xargs)
-    # getting a random index from 0-NUM_ART. (using gshuf instead of $RANDOM for POSIX compliance)
-    random_index=$(shuf -i 1-"$NUM_ART" -n 1)
-    random_pokemon=$(sed "$random_index"'q;d' "$PROGRAM_DIR/nameslist.txt")
-    echo "$random_pokemon"
+  # total number of art files present
+  NUM_ART=$(find "$POKEART_DIR"|wc -l| xargs)
+  # getting a random index from 0-NUM_ART. (using gshuf instead of $RANDOM for POSIX compliance)
+  random_index=$(shuf -i 1-"$NUM_ART" -n 1)
+  random_pokemon=$(sed "$random_index"'q;d' "$PROGRAM_DIR/nameslist.txt")
+  echo "$random_pokemon"
 
-    # print out the pokemon art for the pokemon
-    cat "$POKEART_DIR/$random_pokemon.txt"
+  # print out the pokemon art for the pokemon
+  cat "$POKEART_DIR/$random_pokemon.txt"
 }
 
 _show_pokemon_by_name(){
-    pokemon_name=$1
-    echo "$pokemon_name"
-    # Error handling. Can't think of a better way to do it
-    cat "$POKEART_DIR/$pokemon_name.txt" 2>/dev/null || echo "Invalid pokemon"
+  pokemon_name=$1
+  echo "$pokemon_name"
+  # Error handling. Can't think of a better way to do it
+  cat "$POKEART_DIR/$pokemon_name.txt" 2>/dev/null || echo "Invalid pokemon"
 }
 
 _list_pokemon_names(){
-    less < "$PROGRAM_DIR/nameslist.txt"
+  less < "$PROGRAM_DIR/nameslist.txt"
 }
 
 # Handling command line arguments
 case "$#" in
-    0)
-        # display help if no arguments are given
+  0)
+    # display help if no arguments are given
+    _help
+    ;;
+  1)
+    # Check flag and show appropriate output
+    case $1 in
+      -h | --help | help)
         _help
         ;;
-    1)
-        # Check flag and show appropriate output
-        case $1 in
-            -h | --help | help)
-                _help
-                ;;
-            -r | --random | random)
-                _show_random_pokemon
-                ;;
-            -l | --list | list)
-                _list_pokemon_names
-                ;;
-            *)
-                echo "Input error."
-                exit 1
-                ;;
-        esac
+      -r | --random | random)
+        _show_random_pokemon
         ;;
-
-    2)
-        if [ "$1" = '-n' ]||[ "$1" = '--name' ]||[ "$1" = 'name' ]; then
-            _show_pokemon_by_name "$2"
-        else
-            echo "Input error"
-            exit 1
-        fi
+      -l | --list | list)
+        _list_pokemon_names
         ;;
-    *)
-        echo "Input error, too many arguments."
+      *)
+        echo "Input error."
         exit 1
         ;;
+    esac
+    ;;
+
+  2)
+    if [ "$1" = '-n' ]||[ "$1" = '--name' ]||[ "$1" = 'name' ]; then
+      _show_pokemon_by_name "$2"
+    else
+      echo "Input error"
+      exit 1
+    fi
+    ;;
+  *)
+    echo "Input error, too many arguments."
+    exit 1
+    ;;
 esac
