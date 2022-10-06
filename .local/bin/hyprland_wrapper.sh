95c15ddb44ec4ded76bd4050ae5f5c24e99d3502
Marty1820 <nickm1820@gmail.com>
2022-10-06 17:18:45 -0500

    adding wayland wrappers

---
+++ .local/bin/hyprland_wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ~
+
+export _JAVA_AWT_WM_NONREPARENTING=1
+export XCURSOR_SIZE=24
+export QT_QPA_PLATFORM=wayland
+export MOZ_ENABLE_WAYLAND=1
+export SDL_VIDEODRIVER=wayland
+export XDG_CURRENT_DESKTOP=hyprland
+export XDG_SESSION_DESKTOP=hyprland
+
+exec Hyprland
---
+++ .local/bin/sway_wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ~
+
+export _JAVA_AWT_WM_NONREPARENTING=1
+export XCURSOR_SIZE=24
+export QT_QPA_PLATFORM=wayland
+export MOZ_ENABLE_WAYLAND=1
+export SDL_VIDEODRIVER=wayland
+export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_DESKTOP=sway
+
+exec sway
