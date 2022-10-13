49fd0826a20ba15d90ec41d1091678ebe6449acb
Marty1820 <nickm1820@gmail.com>
2022-10-12 20:27:07 -0500

    add exit on failure/null output

--- .local/bin/hyprland_wrapper.sh
+++ .local/bin/hyprland_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ~
+cd ~ || exit
 
 export _JAVA_AWT_WM_NONREPARENTING=1
 export XCURSOR_SIZE=24
@@ -10,4 +10,4 @@ export SDL_VIDEODRIVER=wayland
 export XDG_CURRENT_DESKTOP=hyprland
 export XDG_SESSION_DESKTOP=hyprland
 
-exec Hyprland
+exec Hyprland >> /dev/null
--- .local/bin/sway_wrapper.sh
+++ .local/bin/sway_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ~
+cd ~ || exit
 
 export _JAVA_AWT_WM_NONREPARENTING=1
 export XCURSOR_SIZE=24
@@ -10,4 +10,4 @@ export SDL_VIDEODRIVER=wayland
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 
-exec sway
+exec sway >> /dev/null
