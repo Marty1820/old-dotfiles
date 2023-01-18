a935dd38492b91309dbbf6cc5f2bf48311842aef
Marty1820 <nickm1820@gmail.com>
2023-01-18 15:47:34 -0600

    reordered/added a few things

--- .local/bin/hyprland_wrapper.sh
+++ .local/bin/hyprland_wrapper.sh
@@ -2,22 +2,21 @@
 
 cd ~ || exit
 
-# XDG Specifications
+# XDG Specification
 export XDG_CURRENT_DESKTOP=Hyprland
+export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=Hyprland
-
 # QT Variables
-export QT_QPA_PLATFORM=wayland
-
-# Toolkit Backend
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_QPA_PLATFORM="wayland;xcb"
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+# Toolkit Backend Variables
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
+export CLUTTER_BACKEND="wayland"
 export GDK_BACKEND=wayland,x11
-
-# Theming
-export XCURSOR_SIZE=24
-
-# Misc.
+# Theming Related Variables
 export MOZ_ENABLE_WAYLAND=1
+export XCURSOR_SIZE=24
 
 exec Hyprland >>/dev/null
--- .local/bin/sway_wrapper.sh
+++ .local/bin/sway_wrapper.sh
@@ -2,12 +2,21 @@
 
 cd ~ || exit
 
-export _JAVA_AWT_WM_NONREPARENTING=1
-export XCURSOR_SIZE=24
-export QT_QPA_PLATFORM=wayland
-export MOZ_ENABLE_WAYLAND=1
-export SDL_VIDEODRIVER=wayland
+# XDG Specification
 export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=sway
+# QT Variables
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_QPA_PLATFORM="wayland;xcb"
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+# Toolkit Backend Variables
+export SDL_VIDEODRIVER=wayland
+export _JAVA_AWT_WM_NONREPARENTING=1
+export CLUTTER_BACKEND="wayland"
+export GDK_BACKEND=wayland,x11
+# Theming Related Variables
+export MOZ_ENABLE_WAYLAND=1
+export XCURSOR_SIZE=24
 
 exec sway >>/dev/null
