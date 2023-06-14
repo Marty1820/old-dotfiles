65bf62668e7c5cf63173c37d56f1658da4a9cc73
Marty1820 <nickm1820@gmail.com>
2023-06-13 20:16:58 -0500

    minor tweaks

--- .config/wezterm/keys.lua
+++ .config/wezterm/keys.lua
@@ -1,41 +1,35 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 return {
-  -- Tab Navigation
-  { key = 'LeftArrow',  mods = 'CTRL', action = act.ActivateTabRelative(-1) },
-  { key = 'RightArrow', mods = 'CTRL', action = act.ActivateTabRelative(1) },
-  { key = '1', mods = 'CTRL', action = act.ActivateTab(0) },
-  { key = '2', mods = 'CTRL', action = act.ActivateTab(1) },
-  { key = '3', mods = 'CTRL', action = act.ActivateTab(2) },
-  { key = '4', mods = 'CTRL', action = act.ActivateTab(3) },
-  { key = '5', mods = 'CTRL', action = act.ActivateTab(4) },
-  -- Tab/Pane Creation
-  { key = 't', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain', },
-  { key = 'T',
-    mods = 'CTRL|SHIFT',
-    action = act.SplitPane {
-      direction = 'Right',
-      size = { Percent = 50 },
-    },
-  },
-  -- Tab/Pane Deletion
-  { key = 'w',
-    mods = 'CTRL',
-    action = act.CloseCurrentTab {
-      confirm = false
-    },
-  },
-  { key = 'w',
-    mods = 'ALT',
-    action = wezterm.action.CloseCurrentPane {
-      confirm = false
-    },
-  },
-  -- Terminal Navigation
-  { key = 'PageUp', mods = '', action = act.ScrollByPage(-1) },
-  { key = 'PageDown', mods = '', action = act.ScrollByPage(1) },
-  { key = 'Home', mods = '', action = act.ScrollToTop },
-  { key = 'End', mods = '', action = act.ScrollToBottom },
+	-- Tab Navigation
+	{ key = "LeftArrow", mods = "CTRL", action = act.ActivateTabRelative(-1) },
+	{ key = "RightArrow", mods = "CTRL", action = act.ActivateTabRelative(1) },
+	{ key = "1", mods = "CTRL", action = act.ActivateTab(0) },
+	{ key = "2", mods = "CTRL", action = act.ActivateTab(1) },
+	{ key = "3", mods = "CTRL", action = act.ActivateTab(2) },
+	{ key = "4", mods = "CTRL", action = act.ActivateTab(3) },
+	{ key = "5", mods = "CTRL", action = act.ActivateTab(4) },
+	-- Tab/Pane Creation
+	{ key = "t", mods = "CTRL", action = act.SpawnTab("CurrentPaneDomain") },
+	{
+		key = "T",
+		mods = "CTRL|SHIFT",
+		action = act.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	-- Tab/Pane Deletion
+	{ key = "w", mods = "CTRL", action = act.CloseCurrentTab({
+		confirm = false,
+	}) },
+	{ key = "w", mods = "ALT", action = wezterm.action.CloseCurrentPane({
+		confirm = false,
+	}) },
+	-- Terminal Navigation
+	{ key = "PageUp", mods = "", action = act.ScrollByPage(-1) },
+	{ key = "PageDown", mods = "", action = act.ScrollByPage(1) },
+	{ key = "Home", mods = "", action = act.ScrollToTop },
+	{ key = "End", mods = "", action = act.ScrollToBottom },
 }
-
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,25 +1,27 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
-title = require('title_bar');
+local title = require("title_bar")
 
 -- All Other Settings
 return {
-  -- Fonts
-  font = wezterm.font 'Hack Nerd Font Mono',
-  font_size	= 12.0,
+	-- Fonts
+	font = wezterm.font("Hack Nerd Font Mono"),
+	font_size = 12.0,
 
-  -- Colors
-  color_scheme = "Dracula (Official)", -- full list @ wezfurlong.org/wezterm/colorschemes/index.html
+	-- Colors
+	color_scheme = "Dracula (Official)", -- full list @ wezfurlong.org/wezterm/colorschemes/index.html
+	tab_bar_at_bottom = false,
+	use_fancy_tab_bar = true,
+	window_decorations = "RESIZE",
 
-  -- Key Assignments
-  keys = require('keys'),
+	-- Keybindings
+	keys = require("keys"),
 
-  -- Appearance/Settings
-  window_background_opacity = 1.0,
-  enable_tab_bar = true,
-  hide_tab_bar_if_only_one_tab = true,
-  scrollback_lines = 5000,
-  adjust_window_size_when_changing_font_size = false,
-  window_close_confirmation = 'NeverPrompt',
+	-- Appearance/Settings
+	window_background_opacity = 1.0,
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	scrollback_lines = 5000,
+	adjust_window_size_when_changing_font_size = false,
+	window_close_confirmation = "NeverPrompt",
 }
-
