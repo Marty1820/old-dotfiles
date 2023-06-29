b2e4edcb45f54f32c9d534943346e4952bc9261b
Marty1820 <nickm1820@gmail.com>
2023-06-29 09:04:49 -0500

    fixed icons

--- .config/dunst/scripts/backlight.sh
+++ .config/dunst/scripts/backlight.sh
@@ -7,52 +7,51 @@
 
 # Gets brightness percent from 'xbacklight'
 get_backlight() {
-  fullnum=$(xbacklight -get)
-  printf %.0f "$fullnum"
+	xbacklight -get | cut -d '.' -f 1
 }
 
 # Changes percent increase/decrease
 # depending on current brightness & direction
 set_curve() {
-  backlight=$(get_backlight)
-  _set_curve_direction="$1"
-  if [ "$_set_curve_direction" = up ]; then
-    if [ "$backlight" -lt 10 ]; then
-      percent=1
-    elif [ "$backlight" -lt 50 ]; then
-      percent=5
-    else
-      percent=10
-    fi
-  else
-    if [ "$backlight" -le 10 ]; then
-      percent=1
-    elif [ "$backlight" -le 50 ]; then
-      percent=5
-    else
-      percent=10
-    fi
-  fi
+	backlight=$(get_backlight)
+	_set_curve_direction="$1"
+	if [ "$_set_curve_direction" = up ]; then
+		if [ "$backlight" -lt 10 ]; then
+			percent=1
+		elif [ "$backlight" -lt 50 ]; then
+			percent=5
+		else
+			percent=10
+		fi
+	else
+		if [ "$backlight" -le 10 ]; then
+			percent=1
+		elif [ "$backlight" -le 50 ]; then
+			percent=5
+		else
+			percent=10
+		fi
+	fi
 }
 
 # Sends notification with dunst and set progress bar
 send_notification() {
-  backlight=$(get_backlight)
-  icon=display-brightness-symbolic
-  # Specialized bar
-  bar=$(seq -s "─" 0 $((backlight / 5)) | sed 's/[0-9]//g')
-  dunstify -i "$icon" --timeout=1600 --replace=2593 --urgency=normal "$backlight    $bar"
+	backlight=$(get_backlight)
+	icon=/usr/share/icons/dracula-icons/16/panel/gpm-brightness-lcd.svg
+	# Specialized bar
+	bar=$(seq -s "─" 0 $((backlight / 5)) | sed 's/[0-9]//g')
+	dunstify -i "$icon" --timeout=1600 --replace=2593 --urgency=normal "$backlight    $bar"
 }
 
 case $1 in
 up)
-  set_curve "up"
-  xbacklight -inc $percent >/dev/null
-  send_notification
-  ;;
+	set_curve "up"
+	xbacklight -inc $percent >/dev/null
+	send_notification
+	;;
 down)
-  set_curve "down"
-  xbacklight -dec $percent >/dev/null
-  send_notification
-  ;;
+	set_curve "down"
+	xbacklight -dec $percent >/dev/null
+	send_notification
+	;;
 esac
--- .config/dunst/scripts/volume.sh
+++ .config/dunst/scripts/volume.sh
@@ -7,52 +7,52 @@
 # $./volume.sh mute
 
 # Icons
-vol_high=audio-volume-high-symbolic
-vol_med=audio-volume-medium-symbolic
-vol_low=audio-volume-low-symbolic
-vol_mute=audio-volume-muted-symbolic
+vol_high=/usr/share/icons/dracula-icons/16/panel/audio-volume-high.svg
+vol_med=/usr/share/icons/dracula-icons/16/panel/audio-volume-medium.svg
+vol_low=/usr/share/icons/dracula-icons/16/panel/audio-volume-low.svg
+vol_mute=/usr/share/icons/dracula-icons/16/panel/audio-volume-muted.svg
 
 # Gets volume percent without the '%' sign
 get_volume() {
-  pamixer --get-volume
+	pamixer --get-volume
 }
 
 # Checks if volume is muted
 is_mute() {
-  pamixer --get-mute | grep true >/dev/null
+	pamixer --get-mute | grep true >/dev/null
 }
 
 send_notification() {
-  volume=$(get_volume)
-  # Specialized bar
-  bar=$(seq -s "─" 0 $((volume / 5)) | sed 's/[0-9]//g')
-  # Send the notification
-  if [ "$volume" = "0" ]; then
-    dunstify -i "$vol_mute" --timeout=1600 --replace=2593 --urgency=normal "$volume    $bar"
-  elif [ "$volume" -lt "30" ]; then
-    dunstify -i "$vol_low" --timeout=1600 --replace=2593 --urgency=normal "$volume    $bar"
-  elif [ "$volume" -lt "80" ]; then
-    dunstify -i "$vol_med" --timeout=1600 --replace=2593 --urgency=normal "$volume    $bar"
-  else
-    dunstify -i "$vol_high" --timeout=1600 --replace=2593 --urgency=normal "$volume    $bar"
-  fi
+	volume=$(get_volume)
+	# Specialized bar
+	bar=$(seq -s "─" 0 $((volume / 5)) | sed 's/[0-9]//g')
+	# Send the notification
+	if [ "$volume" = "0" ]; then
+		dunstify -i "$vol_mute" --timeout=1600 --replace=2593 --urgency=normal "$volume    $bar"
+	elif [ "$volume" -lt "30" ]; then
+		dunstify -i "$vol_low" --timeout=1600 --replace=2593 --urgency=normal "$volume    $bar"
+	elif [ "$volume" -lt "80" ]; then
+		dunstify -i "$vol_med" --timeout=1600 --replace=2593 --urgency=normal "$volume    $bar"
+	else
+		dunstify -i "$vol_high" --timeout=1600 --replace=2593 --urgency=normal "$volume    $bar"
+	fi
 }
 
 case $1 in
 up)
-  pamixer -u -i 1
-  send_notification
-  ;;
+	pamixer -u -i 1
+	send_notification
+	;;
 down)
-  pamixer -u -d 1
-  send_notification
-  ;;
+	pamixer -u -d 1
+	send_notification
+	;;
 mute)
-  pamixer -t
-  if is_mute; then
-    dunstify -i "$vol_mute" --timeout=1600 --replace=2593 --urgency=normal "Mute" -h string:x-dunst-stack-tage:volume
-  else
-    send_notification
-  fi
-  ;;
+	pamixer -t
+	if is_mute; then
+		dunstify -i "$vol_mute" --timeout=1600 --replace=2593 --urgency=normal "Mute" -h string:x-dunst-stack-tage:volume
+	else
+		send_notification
+	fi
+	;;
 esac
