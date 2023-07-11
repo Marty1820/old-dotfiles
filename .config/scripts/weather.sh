47fa9792cb37748176de673f9a02999a41d7c4c0
Marty1820 <nickm1820@gmail.com>
2023-07-10 20:12:40 -0500

    change file location/uses lon & lat

--- .config/scripts/weather.sh
+++ .config/scripts/weather.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
 ## Data dir
-cache_dir="$HOME/.cache"
-cache_file="$cache_dir/weatherdata"
+cache_dir="$HOME/.cache/weather"
+weather_file="$cache_dir/weatherdata"
 
 ## Weather data | openweatherdata file first line key second line id
-KEY=$(awk 'NR == 1' ~/.config/scripts/openweatherdata)
-ID=$(awk 'NR == 2' ~/.config/scripts/openweatherdata)
+FILE="$HOME/.config/scripts/openweatherdata"
+KEY=$(awk 'NR == 1' "$FILE")
+LAT=$(awk 'NR == 2' "$FILE")
+LON=$(awk 'NR == 3' "$FILE")
 UNIT="imperial" # Available options : 'metric' or 'imperial'
 
 ## Make cache dir
@@ -16,102 +18,100 @@ fi
 
 ## Get data
 get_weather_data() {
-	weather=$(curl -sf "http://api.openweathermap.org/data/2.5/weather?APPID=$KEY&id=$ID&units=$UNIT")
-	echo "$weather" > "${cache_file}"
+	weather=$(curl -sf "http://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON&appid=$KEY&units=$UNIT")
+	echo "$weather" >"${weather_file}"
 }
 
 # Pulling info from file
-w_temp=$(jq ".main.temp" < "$cache_file" | cut -d "." -f 1)
-w_ftemp=$(jq ".main.feels_like"  < "$cache_file" | cut -d "." -f 1)
-w_stat=$(jq -r ".weather[].description"  < "$cache_file" | head -1 | sed -e "s/\b\(.\)/\u\1/g")
-w_city=$(jq -r ".name"  < "$cache_file" | head -1)
-w_humid=$(jq -r ".main.humidity"  < "$cache_file" | cut -d "." -f 1)
-w_wind=$(jq -r ".wind" < "$cache_file" | cut -d ":" -f 2 -s | head -1 | sed 's/.$//')
-
+w_temp=$(jq ".main.temp" <"$weather_file" | cut -d "." -f 1)
+w_ftemp=$(jq ".main.feels_like" <"$weather_file" | cut -d "." -f 1)
+w_stat=$(jq -r ".weather[].description" <"$weather_file" | head -1 | sed -e "s/\b\(.\)/\u\1/g")
+w_city=$(jq -r ".name" <"$weather_file" | head -1)
+w_humid=$(jq -r ".main.humidity" <"$weather_file" | cut -d "." -f 1)
+w_wind=$(jq -r ".wind" <"$weather_file" | cut -d ":" -f 2 -s | head -1 | sed 's/.$//')
 
 # Setting icon and hex values
 set_icon() {
-  w_icon_code=$(jq -r ".weather[].icon" < "$cache_file" | head -1)
+	w_icon_code=$(jq -r ".weather[].icon" <"$weather_file" | head -1)
 
-  #Big long if statement of doom
-  if [ "$w_icon_code" = "01d" ]; then
-    w_icon="󰖙"
-    w_hex="#ffb86c"
-  elif [ "$w_icon_code" = "01n" ]; then
-    w_icon="󰖔"
-    w_hex="#bd93f9"
-  elif [ "$w_icon_code" = "02d" ]; then
-    w_icon="󰖕"
-    w_hex="#f1fa8c"
-  elif [ "$w_icon_code" = "02n" ]; then
-    w_icon="󰼱"
-    w_hex="#6272a4"
-  elif [ "$w_icon_code" = "03d" ] || [ "$w_icon_code" = "03n" ] || [ "$w_icon_code" = "04d" ] || [ "$w_icon_code" = "04n" ]; then
-    w_icon="󰖐"
-    w_hex="#bd93f9"
-  elif [ "$w_icon_code" = "09d" ] || [ "$w_icon_code" = "09n" ]; then
-    w_icon="󰖖"
-    w_hex="#8be9fd"
-  elif [ "$w_icon_code" = "10d" ] || [ "$w_icon_code" = "10n" ]; then
-    w_icon="󰼳"
-    w_hex="#8be9fd"
-  elif [ "$w_icon_code" = "11d" ]; then
-    w_icon="󰼲"
-    w_hex="#ffb86c"
-  elif [ "$w_icon_code" = "11n" ]; then
-    w_icon="󰖓"
-    w_hex="#ffb86c"
-  elif [ "$w_icon_code" = "13d" ]; then
-    w_icon="󰼴"
-    w_hex="#8be9fd"
-  elif [ "$w_icon_code" = "13n" ]; then
-    w_icon="󰼶"
-    w_hex="#8be9fd"
-  elif [ "$w_icon_code" = "50d" ]; then
-    w_icon="󰼰"
-    w_hex="#6272a4"
-  elif [ "$w_icon_code" = "50n" ]; then
-    w_icon="󰖑"
-    w_hex="#6272a4"
-  else
-    w_icon="󰼯"
-    w_hex="#ff5555"
-  fi
+	#Big long if statement of doom
+	if [ "$w_icon_code" = "01d" ]; then
+		w_icon="󰖙"
+		w_hex="#ffb86c"
+	elif [ "$w_icon_code" = "01n" ]; then
+		w_icon="󰖔"
+		w_hex="#bd93f9"
+	elif [ "$w_icon_code" = "02d" ]; then
+		w_icon="󰖕"
+		w_hex="#f1fa8c"
+	elif [ "$w_icon_code" = "02n" ]; then
+		w_icon="󰼱"
+		w_hex="#6272a4"
+	elif [ "$w_icon_code" = "03d" ] || [ "$w_icon_code" = "03n" ] || [ "$w_icon_code" = "04d" ] || [ "$w_icon_code" = "04n" ]; then
+		w_icon="󰖐"
+		w_hex="#bd93f9"
+	elif [ "$w_icon_code" = "09d" ] || [ "$w_icon_code" = "09n" ]; then
+		w_icon="󰖖"
+		w_hex="#8be9fd"
+	elif [ "$w_icon_code" = "10d" ] || [ "$w_icon_code" = "10n" ]; then
+		w_icon="󰼳"
+		w_hex="#8be9fd"
+	elif [ "$w_icon_code" = "11d" ]; then
+		w_icon="󰼲"
+		w_hex="#ffb86c"
+	elif [ "$w_icon_code" = "11n" ]; then
+		w_icon="󰖓"
+		w_hex="#ffb86c"
+	elif [ "$w_icon_code" = "13d" ]; then
+		w_icon="󰼴"
+		w_hex="#8be9fd"
+	elif [ "$w_icon_code" = "13n" ]; then
+		w_icon="󰼶"
+		w_hex="#8be9fd"
+	elif [ "$w_icon_code" = "50d" ]; then
+		w_icon="󰼰"
+		w_hex="#6272a4"
+	elif [ "$w_icon_code" = "50n" ]; then
+		w_icon="󰖑"
+		w_hex="#6272a4"
+	else
+		w_icon="󰼯"
+		w_hex="#ff5555"
+	fi
 }
 
 case $1 in
-  --getdata)
-    get_weather_data
-    ;;
-  --icon)
-    set_icon
-    echo "$w_icon"
-    ;;
-  --hex)
-    set_icon
-    echo "$w_hex"
-    ;;
-  --temp)
-    echo "$w_temp"
-    ;;
-  --feel)
-    echo "$w_ftemp"
-    ;;
-  --stat)
-    echo "$w_stat"
-    ;;
-  --city)
-    echo "$w_city"
-    ;;
-  --humid)
-    echo "$w_humid"
-    ;;
-  --wind)
-    echo "$w_wind"
-    ;;
-  --waybar)
-    set_icon
-    printf "{\"text\":\"<span foreground=\\\\\"%s\\\\\">%s</span> %s\", \"alt\":\"%s\", \"tooltip\":\"Real feel: %s\"}\n" "$w_hex" "$w_icon" "$w_temp" "$w_stat" "$w_ftemp"
-    ;;
+--getdata)
+	get_weather_data
+	;;
+--icon)
+	set_icon
+	echo "$w_icon"
+	;;
+--hex)
+	set_icon
+	echo "$w_hex"
+	;;
+--temp)
+	echo "$w_temp"
+	;;
+--feel)
+	echo "$w_ftemp"
+	;;
+--stat)
+	echo "$w_stat"
+	;;
+--city)
+	echo "$w_city"
+	;;
+--humid)
+	echo "$w_humid"
+	;;
+--wind)
+	echo "$w_wind"
+	;;
+--waybar)
+	set_icon
+	printf "{\"text\":\"<span foreground=\\\\\"%s\\\\\">%s</span> %s\", \"alt\":\"%s\", \"tooltip\":\"Real feel: %s\"}\n" "$w_hex" "$w_icon" "$w_temp" "$w_stat" "$w_ftemp"
+	;;
 esac
-
