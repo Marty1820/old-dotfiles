4f3653e144ee6a4dfcc5ca058533b918bbdceabb
Marty1820 <nickm1820@gmail.com>
2024-01-20 20:45:11 -0600

    switched to dashboard for greeter

---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,59 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    local logo = [[
+      ███╗░░░███╗░█████╗░██████╗░████████╗██╗░░░██╗
+      ████╗░████║██╔══██╗██╔══██╗╚══██╔══╝╚██╗░██╔╝
+      ██╔████╔██║███████║██████╔╝░░░██║░░░░╚████╔╝░
+      ██║╚██╔╝██║██╔══██║██╔══██╗░░░██║░░░░░╚██╔╝░░
+      ██║░╚═╝░██║██║░░██║██║░░██║░░░██║░░░░░░██║░░░
+      ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░░░╚═╝░░░
+    ]]
+    require('dashboard').setup({
+      theme = 'hyper',
+      config = {
+        header = vim.split(logo, '\n'),
+        project = { enable = false },
+        shortcut = {
+          {
+            desc = '󰈔 > New file',
+            action = 'ene <BAR> startinsert',
+            key = 'e',
+          },
+          {
+            desc = '󰈞 > Find file',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = '󰤘 > Recent Files',
+            action = 'Telescope oldfiles',
+            key = 'r',
+          },
+          {
+            desc = '󱁻 > Settings',
+            action = 'e $MYVIMRC | :cd %:p:h',
+            key = 's',
+          },
+          {
+            desc = '󰂓 > LazyVim',
+            action = 'Lazy',
+            key = 'l',
+          },
+          {
+            desc = '󰟾 > Mason',
+            action = 'Mason',
+            key = 'm',
+          },
+          {
+            desc = '󰗼 > Quit',
+            action = 'qa',
+            key = 'q',
+          },
+        },
+      },
+    })
+  end,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
