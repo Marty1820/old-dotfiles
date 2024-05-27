0597d1e05bf4ccde5e3ac84af46c34fda84de4d4
Marty1820 <nickm1820@gmail.com>
2024-05-27 17:18:30 +0900

    minor fix

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -2,19 +2,20 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   dependencies = {
+    'MunifTanjim/nui.nvim',
     'rcarriga/nvim-notify',
   },
   config = function()
     require('noice').setup({
       lsp = {
-        overrid = {
+        override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
         },
       },
       presets = {
-        bottom_search = true,
+        bottom_search = false,
         command_palette = true,
         long_message_to_split = true,
         inc_rename = false,
